b7838ee9e535dba5889320e6abcdc1624ba69d4f
Cestercian <yashafaid@gmail.com>
2024-12-17 23:39:31 +0530

    UPDATE 1.2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Import Firebase modules (v9+)
+// Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase, ref, push, set, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
